22fe6fd4eb96b99572fe1751f55472efeb69bcfc
Vikkans <emilsjostrand1000@gmail.com>
2024-05-03 10:27:52 +0200

    Fixade session.clear

--- display.js
+++ display.js
@@ -5,4 +5,6 @@ function init() {
     const score = sessionStorage.getItem("score"); //Gör dina poäng till en variabel
     const scoreTitle = document.getElementById("newText"); //Gör min gamla text till en variabel
     scoreTitle.textContent = `Du fick ${score}/9 rätt`; //Gör att den gamla texten byts ut till den nya med score
+
+    sessionStorage.clear(); //Tar bort all data så att man kan köra quizet igen
 }
